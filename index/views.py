5ec558b15f64988d7236b96e0f7bc6fcab5eb073
even-young-leaf <evenyoungleaf@gmail.com>
2025-09-02 09:09:44 +0800

    第一次提交

---
+++ index/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import *
+def indexView(request):
+    songDynamic = Dynamic.objects.select_related('song')
+    # 热搜歌曲
+    searchs = songDynamic.order_by('-search').all()[:8]
+    # 音乐分类
+    labels = Label.objects.all()
+    # 热门歌曲
+    popular = songDynamic.order_by('plays').all()[:10]
+    # 新歌推荐
+    recommend = Song.objects.order_by('release').all()[:3]
+    # 热门搜索、热门下载
+    downloads = songDynamic.order_by('-downloads').all()[:6]
+    tabs = [searchs[:6],downloads]
+    return render(request,'index.html',locals())
---
+++ music/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path,re_path,include
+from django.views.static import serve
+from django.conf import settings
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("",include("index.urls")),
+    path("ranking.html",include("ranking.urls")),
+    path("play/,include(play.urls"),
+    path("comment/",include("comment.urls")),
+    path("search/",include("search.urls")),
+    path("user/",include("user.urls")),
+    # 定义媒体资源的路由信息
+    re_path("media/(?P<path>.*)",serve,{"document_root": settings.MEDIA_ROOT},name = "media"),
+]
